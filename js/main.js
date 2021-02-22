4bc8324d223d8f5bc7e609df64309f3ac66f4844
SElioenai <44786318+SElioenai@users.noreply.github.com>
2021-02-21 21:18:59 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(".section-one").hover(function () {
+    $(".film-container").toggleClass("one-is-active");
+  });
+  
+  $(".section-two").hover(function () {
+    $(".film-container").toggleClass("two-is-active");
+  });
+
+  
